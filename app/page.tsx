cdf2784861628e88825863e9e2aeb58bdaa27f67
YM202c2100 <y.m202c2100@gmail.com>
2024-09-15 15:48:13 +0900

    画像を選択したらプレビュー画像が表示されるように変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
+"use client"
+
+import { ChangeEvent, useState } from "react";
+
 export default function Home() {
+  const [previewImgUrl, setPreview] = useState<string>()
+
   return (
-    <div></div>
-  );
+    <form action="" method="post">
+      {previewImgUrl ?
+        <img src={previewImgUrl} className="w-[300px] h-auto" /> 
+        :<div>preview</div>
+      }
+      <div>
+        <label 
+          htmlFor="image_uploads" 
+          className="border border-black cursor-pointer"
+        >
+          アップロードする画像を選択してください
+        </label>
+            <input 
+              type="file" 
+              name="image_uploads" 
+              id="image_uploads" 
+              accept=".png, .jpg, .jpeg" 
+              className="opacity-0"
+              onChange={showPreview}
+            />
+        <button type="submit" className="block">送信</button>
+      </div>
+    </form>
+  )
+
+  function showPreview(e: ChangeEvent<HTMLInputElement>){
+    if(e.target.files === null){
+      setPreview(undefined);
+      return
+    }else{
+      const submitedFile = e.target.files[0]
+      const submitedFileUrl = URL.createObjectURL(submitedFile)
+      setPreview(submitedFileUrl)
+    }
+  }
+
 }
