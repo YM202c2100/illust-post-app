88655c8b5e162e93142a3d476333acca8a150ec7
YM202c2100 <y.m202c2100@gmail.com>
2024-09-19 14:16:45 +0900

    送信ボタンのdisabledに関する処理を記述

--- app/components/form/ImageForm.tsx
+++ app/components/form/ImageForm.tsx
@@ -1,10 +1,12 @@
 "use client"
 
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 import { PreviewImage } from "./PreviewImage";
 
 export default function ImageForm() {
   const [previewFile, setPreview] = useState<File>()
+  const submitRef = useRef<HTMLButtonElement>(null);
+
   return (
     <form onSubmit={submitHandler} method="post">
       {previewFile ?<PreviewImage previewFile={previewFile}/>
@@ -15,22 +17,26 @@ export default function ImageForm() {
           htmlFor="image_uploads" 
           className="border border-black cursor-pointer"
         >
-          {previewFile ? "変更する":"アップロードする画像を選択してください"}
+            {previewFile ? "変更する":"アップロードする画像を選択してください"}
         </label>
-            <input 
-              type="file" 
-              name="image_uploads" 
-              id="image_uploads" 
-              accept=".png, .jpg, .jpeg" //ツールを使って指定外のファイルも入力可能なためバリデーション必須
-              className="opacity-0" // デフォルト表記のスタイリングが難しいので代わりにlabelを用いた表示用要素を使用
-              onChange={togglePreview}
-            />
-        <button type="submit" className="block" disabled={previewFile?false:true}>送信</button>
+
+        <input 
+          type="file" 
+          name="image_uploads" 
+          id="image_uploads" 
+          accept=".png, .jpg, .jpeg" //ツールを使って指定外のファイルも入力可能なためバリデーション必須
+          className="opacity-0" // デフォルト表記のスタイリングが難しいので代わりにlabelを用いた表示用要素を使用
+          onChange={togglePreview}
+        />
+
+        <button type="submit" ref={submitRef} className="block" disabled>送信</button>
       </div>
     </form>
   )
 
   async function submitHandler(e:FormEvent<HTMLFormElement>){
+    enableSending(false)
+
     e.preventDefault()
 
     const formData = new FormData(e.currentTarget)
@@ -40,6 +46,13 @@ export default function ImageForm() {
       body:formData
     })
 
+    if(res.ok){
+      const resJson = await res.json();
+      console.log(resJson.message);
+     
+      enableSending(true)
+    }
+
   }
 
   function togglePreview(e: ChangeEvent<HTMLInputElement>){
@@ -47,11 +60,19 @@ export default function ImageForm() {
 
     // ファイル選択画面でキャンセルやescを押した場合はlengthが0になる
     if(e.target.files.length === 0){
+      enableSending(false)
       setPreview(undefined)
     }else{
+      enableSending(true)
       const submitedFile = e.target.files[0]
       setPreview(submitedFile)
     }
+  }
+
+  function enableSending(enable:boolean):void{
+    if(!submitRef.current) return;
+
+    submitRef.current.disabled = !enable
   }
 
 }
