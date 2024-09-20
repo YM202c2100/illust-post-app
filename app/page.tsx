212a7f539a82e9245ee2dd462fb74df8802c4009
YM202c2100 <y.m202c2100@gmail.com>
2024-09-20 18:37:20 +0900

    ルートページ(仮)に各ページへのリンクを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import ImageForm from "./components/imageForm/ImageForm";
+import Link from "next/link";
 
 export default function Home(){
   return(
-    <ImageForm/>
+    <div className="flex flex-col space-y-2">
+      <TempLink pageName="home"/>
+      <TempLink pageName="register"/>
+    </div>
+  )
+}
+
+const TempLink:React.FC<{pageName:string}> = ({pageName})=>{
+  return(
+    <Link href={pageName} className="underline underline-offset-2">{pageName}</Link>
   )
 }
