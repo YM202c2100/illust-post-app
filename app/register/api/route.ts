d4950226ad184eda86a3118d6f177ac5314caf65
YM202c2100 <y.m202c2100@gmail.com>
2024-09-22 13:57:10 +0900

    [fix] cookieがない場合に対応できるようにheadersの指定方法を変更

--- app/register/api/route.ts
+++ app/register/api/route.ts
@@ -4,15 +4,17 @@ import { PHP_ROOT_PATH } from "@/app/api/config"
 export async function POST(req:Request){
   const bodyData = await req.formData()
   const cookie = req.headers.get("cookie")
-  if(!cookie) return;
+  
+  const headers = new Headers()
+  if(cookie){
+    headers.append('Cookie', cookie)
+  }
 
   const res = await fetch(`${PHP_ROOT_PATH}/index/register.php`,{
     method : "post",
     body : bodyData,
     credentials : "include",
-    headers : {
-      "Cookie":cookie
-    }
+    headers : headers
   })
 
   const resJson = await res.json()
