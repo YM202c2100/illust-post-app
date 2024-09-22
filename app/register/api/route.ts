4c081dd3de6a0ce63e1359fa448455a0d25db946
YM202c2100 <y.m202c2100@gmail.com>
2024-09-22 20:16:43 +0900

    [fix] phpサーバーからのset-cookieヘッダーをクライアントに渡すように変更

--- app/register/api/route.ts
+++ app/register/api/route.ts
@@ -1,23 +1,32 @@
-import { NextResponse } from "next/server"
 import { PHP_ROOT_PATH } from "@/app/api/config"
 
 export async function POST(req:Request){
   const bodyData = await req.formData()
   const cookie = req.headers.get("cookie")
   
-  const headers = new Headers()
+  const reqHeaders = new Headers()
   if(cookie){
-    headers.append('Cookie', cookie)
+    reqHeaders.append('Cookie', cookie)
   }
 
   const res = await fetch(`${PHP_ROOT_PATH}/index/register.php`,{
     method : "post",
     body : bodyData,
     credentials : "include",
-    headers : headers
+    headers : reqHeaders
   })
 
-  const resJson = await res.json()
+  const resHeaders = new Headers()
+  const setCookie = res.headers.get("set-cookie")
+  if(setCookie){
+    resHeaders.set("Set-Cookie", setCookie)
+  }
+  const data = await res.json()
 
-  return NextResponse.json(resJson)
+  return new Response(
+    JSON.stringify(data),
+    {
+      headers:resHeaders
+    }
+  )
 }
