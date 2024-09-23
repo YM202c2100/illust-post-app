a69186eb2dd35f9e1e64b7909f9b1750e44be8ef
YM202c2100 <y.m202c2100@gmail.com>
2024-09-23 21:55:35 +0900

    [chore] ルートページにログイン画面へのリンクとログアウトボタンを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { LogoutButton } from "./(userAuth)/components/LogoutButton";
 
 export default function Home(){
   return(
-    <div className="flex flex-col space-y-2">
+    <div className="flex flex-col space-y-2 items-start">
       <TempLink pageName="home"/>
       <TempLink pageName="register"/>
+      <TempLink pageName="login"/>
+      <LogoutButton/>
     </div>
   )
 }
