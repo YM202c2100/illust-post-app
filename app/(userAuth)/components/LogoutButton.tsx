528327cda4cb8bafdd8945f52e6763c0f674d6b4
YM202c2100 <y.m202c2100@gmail.com>
2024-09-23 21:54:25 +0900

    [feat] ログアウト機能を持つボタンを作成

---
+++ app/(userAuth)/components/LogoutButton.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export function LogoutButton(){
+  return(
+    <button onClick={logoutHandler}>ログアウト</button>
+  )
+
+  async function logoutHandler(){
+    const res = await fetch("/logout/api")
+    const resData = await res.json()
+    console.log(resData);
+  }
+}
