b3f5e97fbf73e8432603e76c096d79e2ac581a3c
YM202c2100 <y.m202c2100@gmail.com>
2024-09-24 22:15:31 +0900

    [feat] getメソッドでリクエストが来た時にログインしているかどうかを返すよう関数を追加
    現時点では、簡易的な確認をしているだけであり、必要に応じてPHPサーバーとやりとりするひつようがあるかも

--- app/(userAuth)/login/api/route.ts
+++ app/(userAuth)/login/api/route.ts
@@ -1,6 +1,17 @@
 import { PHP_ROOT_PATH } from "@/app/api/config";
 import { sharedCookieRequest } from "@/app/(userAuth)/api/sharedCookieRequest";
+import { cookies } from "next/headers";
 
 export async function POST(req:Request) {
   return await sharedCookieRequest(req, PHP_ROOT_PATH+"/index/login.php")
+}
+
+export function GET() {
+  const isLogin = haveSessionID()
+  return new Response(JSON.stringify({isLogin:isLogin}))
+}
+
+function haveSessionID(){
+  const cookieStore = cookies()
+  return cookieStore.has("PHPSESSID")
 }
