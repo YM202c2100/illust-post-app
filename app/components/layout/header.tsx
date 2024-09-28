74d3eefe5054fe528793f1b7ca28b4c9d087977a
YM202c2100 <y.m202c2100@gmail.com>
2024-09-28 18:20:49 +0900

    [feat] ログイン中のユーザー情報を参照できるように変更

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -2,18 +2,28 @@ import { cookies } from "next/headers"
 import { LoginButton } from "@/app/features/userAuth/login/components/loginButton"
 import { LogoutButton } from "@/app/features/userAuth/logout/components/logoutButton"
 import Link from "next/link"
+import { PHP_ROOT_PATH } from "@/app/api/config"
+import { getHeaderWithSessId } from "@/app/api/cookieHeader"
 
 export const Header = async ()=>{
-  const isLogin = haveSessionID()
+  const reqHeaders = getHeaderWithSessId()
+  const res = await fetch(PHP_ROOT_PATH+"getUser.php",{
+    credentials:"include",
+    headers:reqHeaders
+  })
+  const user = await res.json()
+
+  console.log(user);
+  
   return(
     <nav>
       <Link href={"/"} className="mx-2">ホーム</Link>
-      {isLogin ? <LogoutButton/> : <LoginButton/>}
+      {(user.length === 0) ? <LoginButton/> : <LogoutButton/>}
+      {user.user_name &&
+        <div>
+          {user.user_name}でログイン中
+        </div>
+      }
     </nav>
   )
-}
-
-export function haveSessionID(){
-  const cookieStore = cookies()
-  return cookieStore.has("PHPSESSID")
 }
