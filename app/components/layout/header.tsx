ded9dd3c755465142ea2550595182a8a69c8b43e
YM202c2100 <y.m202c2100@gmail.com>
2024-09-28 17:47:30 +0900

    [chore] layoutにルートページ用のリンクを追加

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,11 +1,13 @@
 import { cookies } from "next/headers"
 import { LoginButton } from "@/app/features/userAuth/login/components/loginButton"
 import { LogoutButton } from "@/app/features/userAuth/logout/components/logoutButton"
+import Link from "next/link"
 
 export const Header = async ()=>{
   const isLogin = haveSessionID()
   return(
     <nav>
+      <Link href={"/"} className="mx-2">ホーム</Link>
       {isLogin ? <LogoutButton/> : <LoginButton/>}
     </nav>
   )
