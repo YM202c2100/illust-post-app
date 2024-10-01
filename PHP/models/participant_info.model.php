708429516500c24100b27b5e2f3e433e546bd183
YM202c2100 <y.m202c2100@gmail.com>
2024-10-01 13:15:27 +0900

    [fix] submittedにtrueではなく1が渡ってきていたため条件式を変更

--- PHP/models/participant_info.model.php
+++ PHP/models/participant_info.model.php
@@ -8,8 +8,8 @@ class ParticipantModel{
   public $judged_count;
 
   public static function isSubmitted(){
-    $submitted = $_SESSION['submitted'] ?? null;
-    return ($submitted === true);
+    $isSubmitted = $_SESSION['submitted'] ?? null;
+    return ($isSubmitted === 1);
   }
 
   public static function setSubmittedSession($boolean){
