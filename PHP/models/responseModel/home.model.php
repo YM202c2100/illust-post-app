83d003ad3809c4f1ba2153e686cb657f26814374
YM202c2100 <y.m202c2100@gmail.com>
2024-10-05 12:01:18 +0900

    [chore] 継承用モデル用のファイル名を変更

--- PHP/models/responseModel/home.model.php
+++ PHP/models/responseModel/home.model.php
@@ -1,7 +1,7 @@
 <?php
 namespace models;
 
-require_once __DIR__."/jsonSerializable.abstract.php";
+require_once __DIR__."/abstract.model.php";
 
 class HomeModel extends JsonSerializable {
   public $isLogin;
