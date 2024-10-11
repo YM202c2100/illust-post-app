b8c8bf2fe30a3483a8172faca02c67adff340269
YM202c2100 <y.m202c2100@gmail.com>
2024-10-11 14:51:07 +0900

    [refactor] targetIdの使用に伴い、必要なくなったものを削除

--- PHP/db/contests.query.php
+++ PHP/db/contests.query.php
@@ -8,18 +8,6 @@ use PDO;
 
 class ContestsQuery {
   static $targetId;
-  static $currentId;
-  static $nextScheduledId;
-  static $prevContestId;
-
-  public static function setCurrentContestId(){
-    $db = new DbConnection();
-    $sql = "SELECT id
-            FROM contests
-            WHERE NOW() BETWEEN application_start_date AND judge_end_date";
-            
-    static::$currentId = $db->fetch($sql, fetchOne:true);
-  }
 
   public static function fetchCurrentContestId(){
     $db = new DbConnection();
@@ -38,33 +26,6 @@ class ContestsQuery {
             limit 1";
 
     return $db->fetch($sql, fetchOne:true);
-  }
-
-  public static function setNextScheduledId(){
-    $db = new DbConnection();
-    $sql = "SELECT id from contests
-            where now() < application_start_date 
-            order by application_start_date 
-            limit 1";
-
-    static::$nextScheduledId = $db->fetch($sql, fetchOne:true);
-  }
-
-  public static function setPrevContestId(){
-    $db = new DbConnection();
-
-    ContestsQuery::setCurrentContestId();
-    if(empty(ContestsQuery::$currentId)){
-      ContestsQuery::setNextScheduledId();
-    }
-    $latestId = ContestsQuery::$currentId ?? ContestsQuery::$nextScheduledId;
-
-    $sql = "SELECT id from contests
-            where round_num = (
-              select round_num - 1 from contests
-              where id = {$latestId})";
-
-    static::$prevContestId = $db->fetch($sql, [], fetchOne:true);
   }
 
   public static function fetchPrevContestId(){
