a4444aa61b4c629c977c39a424a3edbc0207445e
YM202c2100 <y.m202c2100@gmail.com>
2024-10-13 17:30:23 +0900

    [style] レスポンシブデザインを考慮したレイアウトに変更

--- app/features/home/components/myIllust.tsx
+++ app/features/home/components/myIllust.tsx
@@ -1,15 +1,20 @@
 export const MyIllust:React.FC= ()=>{
   return (
-    <div className="xl:flex justify-center space-x-8">
+    <div className="flex flex-col xl:flex-row justify-center items-center gap-8">
       <div className="w-[90vh] h-[90vh] bg-gray-300 rounded-3xl">
 
       </div>
 
-      <div className="w-[45vh] h-[90vh] space-y-2 flex flex-col gap-1">
-        <div className="flex-grow bg-slate-400 rounded-3xl"></div>
-        <div className="h-[30%] bg-slate-400 rounded-3xl"></div>
-        <div className="h-[40%] bg-slate-400 rounded-3xl"></div>
-        <div className="h-[20%] bg-slate-400 rounded-3xl"></div>
+      <div className="h-[90vh] flex xl:flex-col gap-2">
+        <div className="h-1/2 w-[45vh] bg-slate-300 rounded-2xl">
+
+        </div>
+
+        <div className="h-1/2 w-[45vh] flex flex-col gap-2">
+          <div className="h-[30%] bg-slate-300 rounded-2xl"></div>
+          <div className="flex-grow bg-slate-300 rounded-2xl"></div>
+          <div className="h-[20%] bg-slate-300 rounded-2xl"></div>
+        </div>
       </div>
     </div>
   )
