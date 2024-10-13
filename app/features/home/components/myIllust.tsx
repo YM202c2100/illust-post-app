900ea77835470cb640d1e07c56b73675ce981f61
YM202c2100 <y.m202c2100@gmail.com>
2024-10-13 17:41:49 +0900

    [refactor] 全体的なレイアウトをpage.tsx内に記述するように変更

--- app/features/home/components/myIllust.tsx
+++ app/features/home/components/myIllust.tsx
@@ -1,21 +1,7 @@
 export const MyIllust:React.FC= ()=>{
   return (
-    <div className="flex flex-col xl:flex-row justify-center items-center gap-8">
-      <div className="w-[90vh] h-[90vh] bg-gray-300 rounded-3xl">
-
-      </div>
-
-      <div className="h-[90vh] flex xl:flex-col gap-2">
-        <div className="h-1/2 w-[45vh] bg-slate-300 rounded-2xl">
-
-        </div>
-
-        <div className="h-1/2 w-[45vh] flex flex-col gap-2">
-          <div className="h-[30%] bg-slate-300 rounded-2xl"></div>
-          <div className="flex-grow bg-slate-300 rounded-2xl"></div>
-          <div className="h-[20%] bg-slate-300 rounded-2xl"></div>
-        </div>
-      </div>
+    <div className="w-full h-full">
+      <div>image</div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
-import Image from "next/image";
 import { GET } from "./api/getRequest";
 import { convertToValidSrc } from "./libs/helper";
 import { HomeDataGET } from "./models/home.model";
-import { ContestInfo } from "./features/home/components/ContestInfo";
 import { MyIllust } from "./features/home/components/myIllust";
 
 export default async function Home(){
@@ -21,26 +19,38 @@ export default async function Home(){
   const myIllustSrc = data.submittedFileName ?
                         convertToValidSrc(data.submittedFileName)
                         :null
-  return(
-    <div>
-      <MyIllust/>
-      <div>
-        { myIllustSrc ? 
-            <Image src={myIllustSrc} alt="submitted Illust" width={500} height={500}/>:
-            <div>コンテストに画像を投稿してみよう</div>
-        }
+
+  return(<>
+    <div className="flex flex-col xl:flex-row justify-center items-center gap-8">
+      <div className="w-[90vh] h-[90vh] bg-gray-300 rounded-3xl">
+        <MyIllust/>
       </div>
 
-      <ContestInfo contest={data.contest}/>
-      <div className="flex flex-col space-y-2 items-start">
-        <TempLink pageName="post"/>
-        <TempLink pageName="register"/>
-        <TempLink pageName="login"/>
-        <TempLink pageName="judge"/>
-        <TempLink pageName="ranking"/>
+      <div className="h-[90vh] flex xl:flex-col gap-2">
+        <div className="h-1/2 w-[45vh] bg-slate-300 rounded-2xl">
+        </div>
+
+        <div className="h-1/2 w-[45vh] flex flex-col gap-2">
+          <div className="h-[30%] bg-slate-300 rounded-2xl">
+          </div>
+          <div className="flex-grow bg-slate-300 rounded-2xl">
+
+          </div>
+          <div className="h-[20%] bg-slate-300 rounded-2xl">
+
+          </div>
+        </div>
       </div>
+
     </div>
-  )
+    <div className="flex flex-col space-y-2 items-start">
+      <TempLink pageName="post"/>
+      <TempLink pageName="register"/>
+      <TempLink pageName="login"/>
+      <TempLink pageName="judge"/>
+      <TempLink pageName="ranking"/>
+    </div> 
+  </>)
 }
 
 const TempLink:React.FC<{pageName:string}> = ({pageName})=>{
