eb902e533b9036d3da84fe2c82b1db6041ebc345
YM202c2100 <y.m202c2100@gmail.com>
2024-10-17 08:18:34 +0900

    [feat] ランクを表すアイコンを用意

---
+++ app/components/icons/rank.icon.tsx
@@ -0,0 +1,7 @@
+export const RankIconBronze = ()=>{
+  return(
+    <div className="h-[70%] aspect-square rounded-full bg-zinc-300 p-4 border-[#b46e1d] border-4">
+      <img src="/poll.svg"/>
+    </div>
+  )
+}
