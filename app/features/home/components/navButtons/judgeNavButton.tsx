e51b22e06f7ca302037912b99f460e0acc840d15
YM202c2100 <y.m202c2100@gmail.com>
2024-10-16 15:55:33 +0900

    [style] linkを使った記述に変更し、文字サイズを設定

--- app/features/home/components/navButtons/judgeNavButton.tsx
+++ app/features/home/components/navButtons/judgeNavButton.tsx
@@ -1,18 +1,14 @@
-"use client"
-
-import { useRouter } from "next/navigation"
+import Link from "next/link";
 
 export const JudgeNavButton:React.FC<{limitCanJudge:number|null}> = ({limitCanJudge})=>{
-  const router = useRouter();
-
   return(
-    <div 
-      className="cursor-pointer bg-zinc-800 text-white hover:bg-green-200 hover:text-black w-full h-full rounded-2xl"
-      onClick={()=>router.push("judge")}
+    <Link 
+      href={"judge"}
+      className=" bg-zinc-800 text-white hover:bg-zinc-700 h-full rounded-2xl text-2xl p-2 flex flex-col justify-between"
     >
       <div>ジャッジ</div>
-      <div>残り回数：{limitCanJudge ?? 0}/3</div>
-      <div>{(limitCanJudge===null) && "ジャッジを行うには、コンテストに作品を投稿する必要があります"}</div>
-    </div>
+      <div className="text-end">残り回数：{limitCanJudge ?? 0}/3</div>
+      {(limitCanJudge===null) && <div>ジャッジを行うには、コンテストに作品を投稿する必要があります</div>}
+    </Link>
   )
 }
