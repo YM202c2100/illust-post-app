8283304b23aecda84129862da136c21636699512
YM202c2100 <y.m202c2100@gmail.com>
2024-10-17 20:04:35 +0900

    [refactor] アイコンの大きさは呼び出し側で調整するように変更

--- app/components/icons/rank.icon.tsx
+++ app/components/icons/rank.icon.tsx
@@ -1,6 +1,6 @@
 export const RankIconBronze = ()=>{
   return(
-    <div className="h-[70%] aspect-square rounded-full bg-zinc-300 p-4 border-[#b46e1d] border-4">
+    <div className="w-full h-full rounded-full bg-zinc-300 p-4 border-[#b46e1d] border-4">
       <img src="/poll.svg"/>
     </div>
   )
--- app/features/home/components/navButtons/resultNavButton.tsx
+++ app/features/home/components/navButtons/resultNavButton.tsx
@@ -1,9 +1,13 @@
+import { RankIconBronze } from "@/app/components/icons/rank.icon"
 import Link from "next/link"
 
 export const ResultNavButton:React.FC = ()=>{
   return(
     <Link href={"result"} className="block h-full bg-zinc-800 text-white rounded-2xl">
       <div>コンテスト結果</div>
+      <div className="h-[70%] aspect-square">
+        <RankIconBronze/>
+      </div>
     </Link>
   )
 }
