7728e24bf05c0725b5a1c1a719c1c836ab9f2a9e
YM202c2100 <y.m202c2100@gmail.com>
2024-10-17 23:38:57 +0900

    [fix] tailwindCSSの仕様に合わせて、classNameの変数を定義するように修正

--- app/components/icons/rank.icon.tsx
+++ app/components/icons/rank.icon.tsx
@@ -1,10 +1,10 @@
 export const BronzeIcon = ()=>{
-  return <RankIcon svgSrc="/poll.svg" borderColorHex="#b46e1d"/>
+  return <RankIcon svgSrc="/poll.svg" borderColorHex="border-[#ab7940]"/>
 }
 
 export const RankIcon:React.FC<{svgSrc:string, borderColorHex:string}> = ({svgSrc, borderColorHex})=>{
   return(
-    <div className={`w-full h-full rounded-full bg-zinc-300 p-4 border-[${borderColorHex}] border-4`}>
+    <div className={`w-full h-full rounded-full bg-zinc-300 p-4 ${borderColorHex} border-4`}>
       <img src={svgSrc}/>
     </div>
   )
