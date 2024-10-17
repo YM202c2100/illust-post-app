ee9d09828dc0c423a63e562b7226ebffa5fe689e
YM202c2100 <y.m202c2100@gmail.com>
2024-10-17 23:39:38 +0900

    [feat] 各種ランクアイコンのコンポーネントを作成

--- app/components/icons/rank.icon.tsx
+++ app/components/icons/rank.icon.tsx
@@ -2,6 +2,22 @@ export const BronzeIcon = ()=>{
   return <RankIcon svgSrc="/poll.svg" borderColorHex="border-[#ab7940]"/>
 }
 
+export const SilverIcon = ()=>{
+  return <RankIcon svgSrc="/knight.svg" borderColorHex="border-[#acb2b5]"/>
+}
+
+export const GoldIcon = ()=>{
+  return <RankIcon svgSrc="/bishop.svg" borderColorHex="border-[#fae093]"/>
+}
+
+export const DiamondIcon = ()=>{
+  return <RankIcon svgSrc="/rook.svg" borderColorHex="border-[#a3c2ec]"/>
+}
+
+export const MasterIcon = ()=>{
+  return <RankIcon svgSrc="/queen.svg" borderColorHex="border-[#ab73ce]"/>
+}
+
 export const RankIcon:React.FC<{svgSrc:string, borderColorHex:string}> = ({svgSrc, borderColorHex})=>{
   return(
     <div className={`w-full h-full rounded-full bg-zinc-300 p-4 ${borderColorHex} border-4`}>
