82fedfe2e48e0807001ad4f740e8ee6163ba4fd4
YM202c2100 <y.m202c2100@gmail.com>
2024-10-18 00:03:39 +0900

    [style] ランクアイコンの位置を設定
    aspect-squareがwidth基準でheightを合わせるらしいのでwidth指定するように変更

--- app/features/home/components/navButtons/resultNavButton.tsx
+++ app/features/home/components/navButtons/resultNavButton.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 export const ResultNavButton:React.FC = ()=>{
   return(
-    <Link href={"result"} className="block h-full bg-zinc-800 text-white rounded-2xl">
+    <Link href={"result"} className="h-full bg-zinc-800 text-white rounded-2xl p-2 flex justify-between">
       <div>コンテスト結果</div>
-      <div className="h-[70%] aspect-square">
+      <div className="w-[40%] aspect-square self-center mr-2">
         <BronzeIcon/>
       </div>
     </Link>
