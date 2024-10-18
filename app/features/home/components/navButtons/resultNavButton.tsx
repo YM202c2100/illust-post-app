335b7c6066aba2ca9acd4beecab84192b03ea28d
YM202c2100 <y.m202c2100@gmail.com>
2024-10-19 00:02:25 +0900

    [style] テキストサイズを調整

--- app/features/home/components/navButtons/resultNavButton.tsx
+++ app/features/home/components/navButtons/resultNavButton.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export const ResultNavButton:React.FC<{rankTier:RankTier}> = ({rankTier})=>{
 
   return(
-    <Link href={"result"} className="h-full bg-zinc-800 text-white rounded-2xl p-2 flex justify-between">
+    <Link href={"result"} className="h-full bg-zinc-800 text-2xl text-white rounded-2xl p-2 flex justify-between">
       <div>コンテスト結果</div>
       <div className="w-[40%] aspect-square self-center mr-2">
         <GetIconByRankTier rankTier={rankTier}/>
