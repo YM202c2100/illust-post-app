bdc6ede26bfa61ac2b1e37a40699cdc7e1e64104
YM202c2100 <y.m202c2100@gmail.com>
2024-10-21 14:35:13 +0900

    [chore] コンテストに参加していないユーザーの、ジャッジ残り回数とメッセージを修正

--- app/features/home/components/navButtons/judgeNavButton.tsx
+++ app/features/home/components/navButtons/judgeNavButton.tsx
@@ -7,8 +7,8 @@ export const JudgeNavButton:React.FC<{limitCanJudge:number|null}> = ({limitCanJu
       className=" bg-zinc-800 text-white hover:bg-zinc-700 h-full rounded-2xl text-2xl p-2 flex flex-col justify-between"
     >
       <div>ジャッジ</div>
-      <div className="text-end">残り回数：{limitCanJudge ?? 0}/3</div>
-      {(limitCanJudge===null) && <div>ジャッジを行うには、コンテストに作品を投稿する必要があります</div>}
+      <div className="text-end">残り回数：{limitCanJudge ?? "-"}/3</div>
+      {(limitCanJudge===null) && <div className="text-sm">※ジャッジを行うには、コンテストに作品を投稿する必要があります</div>}
     </Link>
   )
 }
