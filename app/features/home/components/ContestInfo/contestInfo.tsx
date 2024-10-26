42c7ad5da06d35b22a150d122a0adcfce00ca842
YM202c2100 <y.m202c2100@gmail.com>
2024-10-26 16:39:22 +0900

    [style] コンテスト情報を中央揃え

--- app/features/home/components/ContestInfo/contestInfo.tsx
+++ app/features/home/components/ContestInfo/contestInfo.tsx
@@ -7,8 +7,8 @@ export const ContestInfo:React.FC<{contest:ContestData}> = ({contest})=>{
   const formattedJudgePeriod = formatPeriod(judgePeriod)
   
   return(
-    <div className="h-full bg-zinc-800 rounded-2xl text-white text-center pt-4 grid grid-cols-2 landscape:grid-cols-none">
-      <div>
+    <div className="h-full bg-zinc-800 rounded-2xl text-white text-center grid grid-cols-2 landscape:grid-cols-none place-content-center">
+      <div className="place-content-center">
         <div className="md:text-3xl">第{roundNum}回 コンテスト</div>
 
         <div className="md:text-4xl mt-2 relative">
