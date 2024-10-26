2734e509b7d8d0d82321c2f36bd05ed2e8e93d1e
YM202c2100 <y.m202c2100@gmail.com>
2024-10-26 22:17:41 +0900

    [style] モバイル時の文字サイズを縮小
    変更に伴って要素の高さが低くなったものはパディングの大きさを増加

--- app/features/home/components/navButtons/judgeNavButton.tsx
+++ app/features/home/components/navButtons/judgeNavButton.tsx
@@ -4,7 +4,7 @@ export const JudgeNavButton:React.FC<{limitCanJudge:number|null}> = ({limitCanJu
   return(
     <Link 
       href={"judge"}
-      className="py-3 bg-zinc-800 text-white hover:bg-zinc-700 h-full rounded-2xl text-2xl p-2 flex flex-col justify-between"
+      className="py-3 bg-zinc-800 text-white hover:bg-zinc-700 h-full rounded-2xl md:text-2xl p-2 flex flex-col justify-between"
     >
       <div>ジャッジ</div>
       <div className="text-end">残り回数：{limitCanJudge ?? "-"}/3</div>
