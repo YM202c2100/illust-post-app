45e1dff812e370afa9f87b796fc22121250d1a45
YM202c2100 <y.m202c2100@gmail.com>
2024-10-26 12:06:50 +0900

    [style] ビューポートによっては、イラストが画面に収まりきらない場合があるため、cssのorientationとaspect-ratioを用いるレイアウト方法に変更

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,13 @@ export default async function Home(){
                         :null
 
   return(<>
-    <div className="container p-4 mx-auto h-[200vw] md:h-auto grid gap-2 md:gap-6 grid-rows-2 md:grid-rows-none md:grid-cols-3">
-      <div className="bg-orange-300 md:col-span-2 md:aspect-square"></div>
+    <div className="p-4 mx-auto flex flex-col landscape:flex-row items-center justify-center gap-2 landscape:gap-6 ">
+      <div className="w-[90vw] landscape:w-[90vh] aspect-square bg-orange-300">
 
-      <div className="grid gap-2 grid-rows-2 grid-cols-2 md:grid-rows-4 md:grid-cols-none">
-        <div className="bg-red-300 row-span-1 md:row-span-2 col-span-full"></div>
+      </div>
+
+      <div className="w-[90vw] landscape:w-[45vh] aspect-square landscape:aspect-auto landscape:h-[90vh] grid gap-2 grid-rows-2 grid-cols-2 landscape:grid-rows-4 landscape:grid-cols-none">
+        <div className="bg-red-300 row-span-1 landscape:row-span-2 col-span-full"></div>
         <div className="bg-blue-300 row-span-1 col-span-1"></div>
         <div className="bg-green-300 row-span-1 col-span-1"></div>
       </div>
