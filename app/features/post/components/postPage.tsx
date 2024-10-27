e81987b6d80b06203d0ccf6cfba72f17fcd58941
YM202c2100 <y.m202c2100@gmail.com>
2024-10-27 22:22:06 +0900

    [style] postページのレイアウトを仮組みを作成

--- app/features/post/components/postPage.tsx
+++ app/features/post/components/postPage.tsx
@@ -7,7 +7,31 @@ import { useState } from "react"
 
 export const PostPage:React.FC<{imgSrc: string|null}> = ({imgSrc})=>{
   const [isDisable, setButtonDisable] = useState<boolean>(true)
-  return(
+  const [isVisiblePreviousImg, togglePreviousImg] = useState<boolean>(false)
+  return(<>
+    <div className="container mx-auto ">
+      <button 
+        onClick={()=>{togglePreviousImg(!isVisiblePreviousImg)}}
+        className="md:hidden"
+      >
+        現在の応募作品を見る
+      </button>
+      <div className="flex flex-col md:flex-row justify-center items-center bg-gray-300 relative">
+        <div 
+          className={`${isVisiblePreviousImg ? "opacity-100" : "opacity-0"} md:opacity-100 transition-opacity duration-150
+            absolute md:relative 
+            w-[90%] md:w-[40%] aspect-square 
+            bg-red-300`}
+        ></div>
+
+        <div className="hidden md:block flex-grow text-center">
+          →
+        </div>
+
+        <div className="w-[90%] md:w-[40%] aspect-square bg-blue-300"></div>
+      </div>
+    </div>
+
     <div className="flex space-x-10">
       {imgSrc && 
         <div>
@@ -20,5 +44,5 @@ export const PostPage:React.FC<{imgSrc: string|null}> = ({imgSrc})=>{
       <ImageForm setButtonDisable={setButtonDisable}/>
       <PostButton isDisable={isDisable}/>
     </div>
-  )
+  </>)
 }
