bec6d83b74a8c01c8ff2fe8551e19628507ec7aa
YM202c2100 <y.m202c2100@gmail.com>
2024-10-28 10:32:50 +0900

    [style] 作成していた要素をレイアウトに合わせて配置

--- app/features/post/components/postPage.tsx
+++ app/features/post/components/postPage.tsx
@@ -12,11 +12,15 @@ export const PostPage:React.FC<{imgSrc: string|null}> = ({imgSrc})=>{
   if(!imgSrc){
     return(
       <div className="container mx-auto bg-gray-300">
-        <div className="w-[90%] md:w-[90vh] aspect-square mx-auto bg-blue-300"></div>
+        <div className="w-[90%] md:w-[90vh] aspect-square mx-auto bg-blue-300">
+          <ImageForm setButtonDisable={setButtonDisable}/>
+        </div>
+        
+        <PostButton isDisable={isDisable}/>
       </div>
     )
   }else{
-    return(<>
+    return(
       <div className="container mx-auto ">
         <button 
           onClick={()=>{togglePreviousImg(!isVisiblePreviousImg)}}
@@ -24,34 +28,28 @@ export const PostPage:React.FC<{imgSrc: string|null}> = ({imgSrc})=>{
         >
           現在の応募作品を見る
         </button>
+
         <div className="flex flex-col md:flex-row justify-center items-center bg-gray-300 relative">
           <div 
             className={`${isVisiblePreviousImg ? "opacity-100" : "opacity-0"} md:opacity-100 transition-opacity duration-150
-              absolute md:relative 
-              w-[90%] md:w-[40%] aspect-square 
-              bg-red-300`}
-          ></div>
+            absolute md:relative 
+            w-[90%] md:w-[40%] aspect-square 
+            bg-red-300`}
+          >
+            <Image src={imgSrc} alt="submitted Illust" fill style={{objectFit:"contain"}}/>
+          </div>
 
           <div className="hidden md:block flex-grow text-center">
             →
           </div>
 
-          <div className="w-[90%] md:w-[40%] aspect-square bg-blue-300"></div>
-        </div>
-      </div>
-
-      <div className="flex space-x-10">
-        {imgSrc && 
-          <div>
-            <p>提出済みのイラスト</p>
-            <div className="w-[300px] h-[300px] relative">
-              <Image src={imgSrc} alt="submitted Image" fill style={{objectFit:"contain"}}/>
-            </div>
+          <div className="w-[90%] md:w-[40%] aspect-square bg-blue-300">
+            <ImageForm setButtonDisable={setButtonDisable}/>
           </div>
-        }
-        <ImageForm setButtonDisable={setButtonDisable}/>
+        </div>
+
         <PostButton isDisable={isDisable}/>
       </div>
-    </>)
+    )
   }
 }
