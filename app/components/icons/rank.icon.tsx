3b45cc413512e916547e88d761b6f1bbfb63a586
YM202c2100 <y.m202c2100@gmail.com>
2024-10-29 16:07:26 +0900

    [chore] publicフォルダ内にSVGファイル用のフォルダを追加

--- app/components/icons/rank.icon.tsx
+++ app/components/icons/rank.icon.tsx
@@ -16,23 +16,23 @@ export const GetIconByRankTier:React.FC<{rankTier:RankTier}> = ({rankTier})=>{
 }
 
 export const BronzeIcon = ()=>{
-  return <RankIcon svgSrc="/poll.svg" borderColorHex="border-[#ab7940]"/>
+  return <RankIcon svgSrc="/SVG/poll.svg" borderColorHex="border-[#ab7940]"/>
 }
 
 export const SilverIcon = ()=>{
-  return <RankIcon svgSrc="/knight.svg" borderColorHex="border-[#acb2b5]"/>
+  return <RankIcon svgSrc="/SVG/knight.svg" borderColorHex="border-[#acb2b5]"/>
 }
 
 export const GoldIcon = ()=>{
-  return <RankIcon svgSrc="/bishop.svg" borderColorHex="border-[#fae093]"/>
+  return <RankIcon svgSrc="/SVG/bishop.svg" borderColorHex="border-[#fae093]"/>
 }
 
 export const DiamondIcon = ()=>{
-  return <RankIcon svgSrc="/rook.svg" borderColorHex="border-[#a3c2ec]"/>
+  return <RankIcon svgSrc="/SVG/rook.svg" borderColorHex="border-[#a3c2ec]"/>
 }
 
 export const MasterIcon = ()=>{
-  return <RankIcon svgSrc="/queen.svg" borderColorHex="border-[#ab73ce]"/>
+  return <RankIcon svgSrc="/SVG/queen.svg" borderColorHex="border-[#ab73ce]"/>
 }
 
 export const RankIcon:React.FC<{svgSrc:string, borderColorHex:string}> = ({svgSrc, borderColorHex})=>{
