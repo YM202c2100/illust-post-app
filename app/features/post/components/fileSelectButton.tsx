a08fedbd030390e705892f14464d8992610fefd8
YM202c2100 <y.m202c2100@gmail.com>
2024-10-29 14:31:31 +0900

    [style] ファイル選択用のボタンをスタイリング

--- app/features/post/components/fileSelectButton.tsx
+++ app/features/post/components/fileSelectButton.tsx
@@ -1,6 +1,7 @@
 export const FileSelectButton:React.FC = ()=>{
   return (
-    <label htmlFor="image_uploads" className="cursor-pointer">
+    <label htmlFor="image_uploads" 
+      className="p-4 rounded-md bg-rose-400 text-gray-100 cursor-pointer">
       応募する作品を選択
     </label>
   )
