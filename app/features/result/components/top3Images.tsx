f1ecdab7ef647a7df1780d3da797b9e68350971c
YM202c2100 <y.m202c2100@gmail.com>
2024-10-30 14:02:27 +0900

    [refactor] top3画像用のコンポーネントを作成

--- app/features/result/components/top3Images.tsx
+++ app/features/result/components/top3Images.tsx
@@ -1,42 +1,31 @@
 import { ImageWithRP } from "@/app/models/result.model"
-import { OthersImage } from "./othersImage"
+import Image from "next/image"
 
 export const Top3Images:React.FC<{images:ImageWithRP[]}> = ({images})=>{
   
   return(
     <div>
-      <div className="container mx-auto flex flex-col md:flex-row items-center md:items-end gap-4 text-center">
-        <div className="w-[90%]">
-          <p className="text-2xl">1位</p>
-          <div className="flex justify-between">
-            <p>{images[0].user_name}</p>
-            <p>{images[0].rank_points} RP</p>
-          </div>
-          <div className="w-full aspect-square bg-red-300"></div>
-        </div>
-
-        <div className="w-[80%]">
-          <p className="text-2xl">2位</p>
-          <div className="flex justify-between">
-            <p>{images[1].user_name}</p>
-            <p>{images[1].rank_points} RP</p>
-          </div>
-          <div className="w-full aspect-square bg-blue-300"></div>
-        </div>
-
-        <div className="w-[80%]">
-          <p className="text-2xl">3位</p>
-          <div className="flex justify-between">
-            <p>{images[2].user_name}</p>
-            <p>{images[2].rank_points} RP</p>
-          </div>
-          <div className="w-full aspect-square bg-green-300"></div>
-        </div>
+      <p className="text-3xl">上位3作品</p>
+      <div className="container mx-auto flex flex-col md:flex-row items-center md:items-end gap-4">
+        {images.map((image, idx)=>(
+          <Top3Image image={image} order={idx+1}/>
+        ))}
       </div>
-        <p className="text-3xl">上位3作品</p>
-        <div className="grid grid-cols-3">
-          {images.map( image => <OthersImage image={image}/>)}
-        </div>
+    </div>
+  )
+}
+
+const Top3Image:React.FC<{image: ImageWithRP, order:number}> = ({image, order})=>{
+  return(
+    <div key={image.user_name+order} className={`text-center ${(order === 1) ? "w-[90%]" : "w-[80%]"}`}>
+      <p className="text-2xl">{`${order}位`}</p>
+      <div className="flex justify-between">
+        <p>{image.user_name}</p>
+        <p>{image.rank_points} RP</p>
+      </div>
+      <div className="w-full aspect-square relative rounded-xl overflow-hidden">
+        <Image src={`/postedImages/${image.file_name}`} alt="top3 images" fill style={{objectFit:"cover"}}/>
+      </div>
     </div>
   )
 }
