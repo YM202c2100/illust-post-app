afeddff9518ac40a30c87421c6c7eadf3a711414
YM202c2100 <y.m202c2100@gmail.com>
2024-11-02 15:13:40 +0900

    [feat] 自分の投稿結果にランクの変化を示すように変更

--- app/features/result/components/mySubmitResult.tsx
+++ app/features/result/components/mySubmitResult.tsx
@@ -1,26 +1,46 @@
+import { RankIcon } from "@/app/components/icons/rank.icon"
 import { convertToValidSrc } from "@/app/libs/helper"
 import { ResultDataGET } from "@/app/models/pages/result.model"
+import Image from "next/image"
 
 export const MySubmitResult:React.FC<ResultDataGET> = ({rankPosition, totalNumCompetitors, beforeRP, myImage})=>{
   const myImgSrc = convertToValidSrc(myImage.file_name)
-  const percentail = Math.round(rankPosition/totalNumCompetitors*100)
 
   return(
     <div className="text-2xl">
       <div className="text-3xl">投稿した作品</div>
 
-      <div className="grid grid-cols-2 items-center">
+      <div className="md:grid grid-cols-2 items-center">
         <div className="justify-self-end">
           <img src={myImgSrc} width={400} />
         </div>
         <div>
-          <div>RP:{beforeRP ?? "---"}</div>
-          <div>↓</div>
-          <div>RP:{myImage.rank_points}</div>
-          <div>
-            順位:{rankPosition}/{totalNumCompetitors}
+          <div className="flex justify-around items-center">
+            <div className="w-[30%] text-center">
+              <div className="aspect-square relative">
+                <RankIcon rankPoints={beforeRP} />
+              </div>
+              <div className="text-xl">{beforeRP} RP</div>
+            </div>
+
+            <div className="w-[10%] aspect-square relative">
+              <Image src={"/SVG/rightArrow.svg"} 
+                alt="right arrow" 
+                fill style={{objectFit:"cover"}}
+                sizes="20vw"
+              />
+            </div>
+
+            <div className="w-[30%] text-center">
+              <div className="aspect-square relative">
+                <RankIcon rankPoints={myImage.rank_points} />
+              </div>
+              <div className="text-xl">{myImage.rank_points} RP</div>
+            </div>
           </div>
-          <div>上位:{percentail}%</div>
+          <div className="text-center">
+            順位: {rankPosition}/{totalNumCompetitors}
+          </div>
         </div>
       </div>
     </div>
