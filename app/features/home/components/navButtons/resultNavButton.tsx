1cf04616aa97f691a062cd9ff8ec4992232699ee
YM202c2100 <y.m202c2100@gmail.com>
2024-11-04 13:40:35 +0900

    [style] 他のnavButtonと同様にホバー時に色が変わるように変更

--- app/features/home/components/navButtons/resultNavButton.tsx
+++ app/features/home/components/navButtons/resultNavButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export const ResultNavButton:React.FC<{rankPoints:number|null}> = ({rankPoints})=>{
   return(
-    <Link href={"result"} className="h-full bg-zinc-800 md:text-2xl text-white rounded-2xl p-2 flex justify-between">
+    <Link href={"result"} className="h-full bg-zinc-800 hover:bg-zinc-700 md:text-2xl text-white rounded-2xl p-2 flex justify-between">
       <div>コンテスト結果</div>
       <div className="w-[40%] aspect-square self-center mr-2">
         <RankIcon rankPoints={rankPoints}/>
