978bc239e7cbb679a50383e1c075d7e9c45de016
YM202c2100 <y.m202c2100@gmail.com>
2024-11-04 16:58:51 +0900

    [chore] 絶対に表示されるものを先に記述するように修正

--- app/features/result/components/othersImage.tsx
+++ app/features/result/components/othersImage.tsx
@@ -8,33 +8,37 @@ export const OthersImage:React.FC<{
   image:ImageWithRP
 }> = ({image})=>{
   const [isEnlarged, setIsEnlarged] = useState<boolean>(false)
-  return(<>
-    {isEnlarged && 
-      <div 
-        className="fixed inset-0 z-10 bg-enlarged-view flex justify-center items-center cursor-zoom-out"
-        onClick={()=>{setIsEnlarged(false)}}
-      >
-        <div className="w-[98%] h-[98%] relative">
-          <Image src={`/postedImages/${image.file_name}`} 
-            alt="others submission" 
-            fill style={{objectFit:"contain"}}
-            sizes="100vw"
-          />
-        </div>
+  return(
+    <div>
+      <div className="flex justify-between mx-2">
+        <p>{image.user_name}</p>
+        <p>{image.rank_points} RP</p>
       </div>
-    }
-    <div className="flex justify-between mx-2">
-      <p>{image.user_name}</p>
-      <p>{image.rank_points} RP</p>
+
+      <div className="w-full aspect-square relative rounded-xl overflow-hidden">
+        <Image src={`/postedImages/${image.file_name}`} 
+          alt="others submission" 
+          fill style={{objectFit:"cover"}}
+          sizes="(max-width: 768px) 90vw, 40vw"
+          className="cursor-zoom-in"
+          onClick={()=>{setIsEnlarged(true)}}
+        />
+      </div>
+
+      {isEnlarged && 
+        <div 
+          className="fixed inset-0 z-10 bg-enlarged-view flex justify-center items-center cursor-zoom-out"
+          onClick={()=>{setIsEnlarged(false)}}
+        >
+          <div className="w-[98%] h-[98%] relative">
+            <Image src={`/postedImages/${image.file_name}`} 
+              alt="others submission" 
+              fill style={{objectFit:"contain"}}
+              sizes="100vw"
+            />
+          </div>
+        </div>
+      }
     </div>
-    <div className="w-full aspect-square relative rounded-xl overflow-hidden">
-      <Image src={`/postedImages/${image.file_name}`} 
-        alt="others submission" 
-        fill style={{objectFit:"cover"}}
-        sizes="(max-width: 768px) 90vw, 40vw"
-        className="cursor-zoom-in"
-        onClick={()=>{setIsEnlarged(true)}}
-      />
-    </div>
-  </>)
+  )
 }
