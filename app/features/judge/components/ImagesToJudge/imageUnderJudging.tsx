7fbc258b2b7f0617940f819988cf2b64aef881ed
YM202c2100 <y.m202c2100@gmail.com>
2024-11-06 22:23:17 +0900

    [style] 選択時の拡大表示を中央寄せに変更し、ease-inを指定

--- app/features/judge/components/ImagesToJudge/imageUnderJudging.tsx
+++ app/features/judge/components/ImagesToJudge/imageUnderJudging.tsx
@@ -14,8 +14,8 @@ export const ImageUnderJudging:React.FC<ImageUnderJudgingProps> = ({thisSide, se
       className={`snap-center 
         flex-shrink-0 ${(selectedSide===thisSide) ? "md:flex-grow-1.2":"md:flex-grow"}
         w-full md:w-auto 
-        md:flex flex-col justify-end
-        transition-[flex-grow] bg-red-500`}
+        md:flex flex-col justify-center
+        transition-[flex-grow] ease-in bg-red-500`}
       onClick={()=>{dispatchSelectedSide({type:thisSide})}}
     >
       <div className="w-full aspect-square bg-red-300"></div>
