57b207bc7d28f654a98fa11f8844ccb1e9a3ec28
YM202c2100 <y.m202c2100@gmail.com>
2024-11-22 22:46:37 +0900

    [style] 作品を投稿していない場合はリンクによって遷移できないように変更
    背景色も変更

--- app/features/home/components/navButtons/judgeNavButton.tsx
+++ app/features/home/components/navButtons/judgeNavButton.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
 export const JudgeNavButton:React.FC<{limitCanJudge:number|null}> = ({limitCanJudge})=>{
+  const isSubmitted = (limitCanJudge === null)
   return(
     <Link 
       href={"judge"}
-      className="py-5 bg-zinc-800 text-white hover:bg-zinc-700 h-full rounded-2xl md:text-2xl p-2 flex flex-col"
+      className={`py-5 text-white h-full rounded-2xl md:text-2xl p-2 flex flex-col ${isSubmitted ? "bg-zinc-800 hover:bg-zinc-700":"pointer-events-none bg-slate-400"}`}
     >
       <div className="grid grid-cols-2 flex-grow">
         <div className="text-center landscape:text-left">ジャッジ</div>
         <div className="text-center self-end">残り回数：{limitCanJudge ?? "-"}/3</div>
       </div>
-      {(limitCanJudge===null) && <div className="text-xs mt-4">※ジャッジを行うには、コンテストに作品を投稿する必要があります</div>}
+      {!isSubmitted && <div className="text-xs mt-4">※ジャッジを行うには、コンテストに作品を投稿する必要があります</div>}
     </Link>
   )
 }
