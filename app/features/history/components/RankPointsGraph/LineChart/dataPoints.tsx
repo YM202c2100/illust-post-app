dd5a66739483a6a0d71ec3e534894ad359f51118
YM202c2100 <y.m202c2100@gmail.com>
2024-12-06 09:38:34 +0900

    [feat] グラフ上のデータ点を描画するコンポーネントを作成

---
+++ app/features/history/components/RankPointsGraph/LineChart/dataPoints.tsx
@@ -0,0 +1,15 @@
+import { HistoryElem } from "@/app/models/pages/history.model"
+
+export type DataPointsProps = {
+  history: HistoryElem[],
+  dataPointsSpacing: number
+  getPositionY: (value:number)=>number
+}
+
+export const DataPoints:React.FC<DataPointsProps> = ({history, dataPointsSpacing, getPositionY})=>{
+  return <>
+    {history.map((history, i) => (
+      <circle cx={(i+1)*dataPointsSpacing} cy={getPositionY(history.rankPoints)} r={4}/>
+    ))}
+  </>
+}
