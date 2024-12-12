2d878145e2e6629f6eef3fbae07e640eb06b4b90
YM202c2100 <y.m202c2100@gmail.com>
2024-12-12 23:04:39 +0900

    [feat] グラフ上の点をクリックすると、過去の結果の詳細を表示するように変更

--- app/features/history/components/HistoryDetail/historyDetail.tsx
+++ app/features/history/components/HistoryDetail/historyDetail.tsx
@@ -1,8 +1,22 @@
+import { convertToValidSrc } from "@/app/libs/helper"
 import { HistoryElem } from "@/app/models/pages/history.model"
+import Image from "next/image"
 
 export const HistoryDetail:React.FC<{selectedHistory:HistoryElem}> = ({selectedHistory})=>{
-  console.log(selectedHistory);
   return(
-    <div></div>
+    <div>
+      <div className="relative">
+        <Image src={convertToValidSrc(selectedHistory.filePath)}
+          alt="submitted illust"
+          fill
+          style={{objectFit:"contain"}}
+        />
+      </div>
+      <div>
+        <p>第{selectedHistory.contest.roundNum}回コンテスト結果</p>
+        <p>お題：{selectedHistory.contest.subject}</p>
+        <p>最終ランクポイント：{selectedHistory.rankPoints}</p>
+      </div>
+    </div>
   )
 }
