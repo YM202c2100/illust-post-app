b73a5028791fac0b76dc6e4f6f49b1e81bd1556c
YM202c2100 <y.m202c2100@gmail.com>
2024-12-18 16:58:27 +0900

    [feat] 文字に関するスタイルを付与

--- app/features/history/components/HistoryDetail/historyDetail.tsx
+++ app/features/history/components/HistoryDetail/historyDetail.tsx
@@ -17,7 +17,7 @@ export const HistoryDetail:React.FC<{selectedHistory:HistoryElem}> = ({selectedH
           style={{objectFit:"contain"}}
         />
       </div>
-      <div>
+      <div className="text-2xl text-center my-auto">
         <p>{formatDate(selectedHistory.contest.judgeEndDate)}</p>
         <p>第{selectedHistory.contest.roundNum}回コンテスト結果</p>
         <p>お題：{selectedHistory.contest.subject}</p>
