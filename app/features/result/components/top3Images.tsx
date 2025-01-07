d77e6ebbb94d85ebb9835fa95b6d18f46aa043f6
YM202c2100 <y.m202c2100@gmail.com>
2025-01-07 23:14:36 +0900

    [fix] keyの指定の仕方を間違えていたため修正

--- app/features/result/components/top3Images.tsx
+++ app/features/result/components/top3Images.tsx
@@ -8,7 +8,7 @@ export const Top3Images:React.FC<{images:ImageWithRP[]}> = ({images})=>{
       <p className="text-3xl">上位3作品</p>
       <div className="flex flex-col md:flex-row items-center md:items-end gap-4">
         {images.map((image, idx)=>(
-          <Top3Image image={image} order={idx+1}/>
+          <Top3Image image={image} order={idx+1} key={image.user_name+idx}/>
         ))}
       </div>
     </div>
@@ -17,7 +17,7 @@ export const Top3Images:React.FC<{images:ImageWithRP[]}> = ({images})=>{
 
 const Top3Image:React.FC<{image: ImageWithRP, order:number}> = ({image, order})=>{
   return(
-    <div key={image.user_name+order} className={`text-center ${(order === 1) ? "w-[90%]" : "w-[80%]"}`}>
+    <div className={`text-center ${(order === 1) ? "w-[90%]" : "w-[80%]"}`}>
       <p className="text-2xl">{`${order}位`}</p>
       <OthersImage image={image}/>
     </div>
